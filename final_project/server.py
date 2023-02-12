c415957b46170571e5be5f9fd6351eabe5591156
shariqazeem <shariqshaukat786@gmail.com>
2023-02-12 17:39:33 -0500

    Final changes

--- final_project/server.py
+++ final_project/server.py
@@ -1,22 +1,26 @@
+import machinetranslation
 from machinetranslation import translator
 from flask import Flask, render_template, request
 import json
 
 app = Flask("Web Translator")
 
-@app.route("/englishToFrench")
+@app.route("/englishToFrench", methods=["GET"])
 def english_to_french():
-    textToTranslate = request.args.get('textToTranslate')
-    # Write your code here
-    french_text = machinetranslation.translate_text(textToTranslate)
-    return "Translated text to French"
+    english_text = request.args.get("textToTranslate")
+    if not english_text:
+        return "textToTranslate parameter is missing in the request"
+    french_text = translator.english_to_french(english_text)
+    return french_text
 
-@app.route("/frenchToEnglish")
+@app.route("/frenchToEnglish", methods=["GET"])
 def french_to_english():
-    textToTranslate = request.args.get('textToTranslate')
-    # Write your code here
-     english_text = machinetranslation.translate_text(textToTranslate, target_language='en')
-    return "Translated text to English"
+    french_text = request.args.get("textToTranslate")
+    if not french_text:
+        return "textToTranslate parameter is missing in the request"
+    english_text = translator.french_to_english(french_text)
+    return english_text
+
 
 @app.route("/")
 def renderIndexPage():
